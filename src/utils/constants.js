c66501282bbc2543f14087d316eecd62bd8ebc5c
Sourav hazra <souravhazra118@gmail.com>
2024-02-13 23:36:13 +0530

    Search ber done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ export const LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/c/cd/Log
 
 export const FOOD_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const FOOTER_URL = "https://cdn.pixabay.com/photo/2020/06/30/14/37/facebook-5356593_960_720.png";
+export const FOOTER_URL = "https://cdn.pixabay.com/photo/2020/06/30/14/37/facebook-5356593_960_720.png"; 
