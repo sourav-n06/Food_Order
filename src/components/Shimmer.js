48764c1654a7e1d37a515c7f44dad5f5e558776b
Sourav hazra <souravhazra118@gmail.com>
2024-02-15 23:19:29 +0530

    updated

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -15,7 +15,7 @@ const Shimmer = () => {
             </div>
             <div className="inside-shimmer">
                 <div className="img-shimmer"></div>
-                <div className="txt-shimmer-large"></div>
+                <div className="txt-shimmer-large"></div> 
                 <div className="txt-shimmer"></div>
 
             </div>
